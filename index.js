8820d9288391c224aeff9f0fcdd332f0eb512aee
haroon nadir <haroonnadir143@gmail.com>
2025-12-02 05:57:47 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,127 @@
+
+// // for (var i = 0; i < 3; i++) {
+// // }
+// // console.log(i);
+
+
+// // for (let j = 0; j < 3; j++){
+// //     console.log(j); 
+// // }
+
+// let a = "123"; 
+// // string
+// let b = 123;
+// // number
+// let c = true;
+// // boolean
+// let d;
+// // undefine
+// let e = null;
+// // number
+// let f = NaN;
+// // note a number
+// let g = {name: "Ali"};
+// // object
+// let h = ["apple", "mango"];
+// // object in arrays
+
+// console.log(a,b,c,d,e,f,g,h);
+
+
+// console. log(typeof null); 
+// // number  object
+// console. log(typeof NaN);
+// // number
+// console. log(typeof []);
+// // array  object
+// console. log(typeof 0);
+// // number 
+// console. log (typeof undefined);
+// // undefined
+
+
+// console. log("5" + 3); 
+// // string 53 
+// console. log("5" - 3);
+// // number 2 
+// console. log("5" * "2");
+// // number 10 
+// console. log("5" + true);
+// // string 5 true
+// console. log(true + true);
+// // number 2 true     1+1= 2 
+// console. log("10" - true);
+// // number 10        10-1=9
+// console. log("10" + null);
+// // string 10null   10null
+// console. log("10" - null);
+// // number 10null    10 
+
+// console. log(10 + true);
+
+// console. log (Number ("123" ));
+// // numb 123
+// console. log (Number ("abc"));
+// // nan
+// console. log(String (50));
+// // String 50 
+// console. log (Boolean(""));
+// // false
+// console. log(Boolean(" "));
+// // true
+// console. log (Boolean(0));
+// // f
+// console. Log(Boolean([]));
+// // t
+// console. log (Boolean());
+// // t
+
+
+// "" ''
+// nall
+// false
+// 0
+// undefined
+// 0n bigint
+// -0
+// if ("0") {
+// console. log("I am TRUE");
+// } else i
+// console. log("I am FALSE");
+
+// "55"
+// console.log("55"*1)
+// // true
+// // console.log(true*1)
+// console.log(+true)
+// // false
+// // console.log(false-false)
+// console.log(false*1)
+// console.log(+false)
+
+// // "123abc" ?
+// console.log("123abc"*1)
+// console.log(+"123abc")
+
+
+console. log([] == 0);
+// t
+console. log([] == "");
+// t
+console. log ([1] == 1);
+// t
+console. log ([1,2] == "1,2");
+// f       t
+console. log([] == false);
+//t 
+
+Falsy: "", 0, false, null, undefined, NaN
+Truthy: " ", [], {}, "0", "false"
+
+// console. log (Number (null));
+// console. log (Number (undefined));
+// console. log (Number(false));
+// console. log (Number (true));
+// console. log (Number (" "));
+// console. log (Number (""));
+// console. log(Number (" "));
